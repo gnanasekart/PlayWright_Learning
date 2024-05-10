2df1ea77ebfb09c7ff20cd802cb6214e0ac4ca27
Admin <gnanasekar69@hotmail.com>
2024-05-10 22:45:35 +0530

    maximize browser

--- src/test/java/UseCase/LaunchBrowser.java
+++ src/test/java/UseCase/LaunchBrowser.java
@@ -1,22 +1,29 @@
 package UseCase;
 
-import com.microsoft.playwright.Browser;
-import com.microsoft.playwright.BrowserType;
-import com.microsoft.playwright.Page;
-import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.*;
 
 public class LaunchBrowser {
 
 
     public static void main(String[] args) {
         try (Playwright pw = Playwright.create()) {
+
+            //Set browser as headless = false
             BrowserType.LaunchOptions headedBrowser = new BrowserType.LaunchOptions().setHeadless(false);
+            //new chromium browser
             Browser browser = pw.chromium().launch(headedBrowser);
+            //new page inside the browser
             Page page = browser.newPage();
+            //new navigating page
             page.navigate("https://google.co.in");
+            //printing page title
             System.out.println(page.title());
+            //close the web page
             page.close();
+            //close the playwright object
             pw.close();
         }
     }
+
+
 }
